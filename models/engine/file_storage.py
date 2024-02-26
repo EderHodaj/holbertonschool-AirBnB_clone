09a89cea982ceaa786664bc2b108189ddd1cba83
eneamilo91 <eneamilo514@gmail.com>
2024-02-26 23:23:38 +0000

    added file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,24 +6,35 @@ Create FileStorage class
 
 
 class FileStorage:
-    def __init__(self):
-        self.__file_path = ""
-        self.__objects = {}
+    """ class about storing objects to json file"""
+
+    __file_path = "file.json"
+    __objects = {}
   
     def all(self):
-        return self.__objects
+        """return all key value pairs of the dictionary"""
+        return FileStorage.__objects
 
     def new(self, obj):
-        self.__objects["BaseModel.id"] = "obj"
+        """adds a new object as a value to the dictionary"""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        with open(self.__file_path, "w") as json_file:
-            json.dump(self.__objects, json_file)
+        """serializes objects to json file"""
+        json_dict = {}
+        for key, value in FileStorage.__objects.items():
+            json_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, "w") as json_file:
+            json.dump(json_dict, json_file)
 
     def reload(self):
-        if self.__file_path:
-            with open(self.__file_path, "r") as json_file:
-                json.load(json_file)
-        else:
-            pass
-
+        """deserializes dictionaries from json file"""
+        try:
+            with open(FileStorage.__file_path, "r") as json_file:
+                from_json = json.load(json_file)
+                for key, value in from_json.items():
+                    cls_name = globals()[value["__class__"]]
+                    FileStorage.__objects[key] = cls_name(**value)
+        except FileNotFoundError:
+            return

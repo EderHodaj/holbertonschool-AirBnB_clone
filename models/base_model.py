ef953c192e0e9920936e4a6c17634b02575bd34d
EderHodaj <eder0302@gmail.com>
2024-02-26 11:19:26 +0000

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -8,8 +8,8 @@ class BaseModel:
     '''Createing the class attributes'''
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now().isoformat()
-        self.updated_at = datetime.datetime.now().isoformat()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         return f"[BaseModel] ({self.id}) {self.__dict__}"
@@ -18,5 +18,7 @@ class BaseModel:
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
+        self.created_at = datetime.datetime.now().isoformat()
+        self.updated_at = datetime.datetime.now().isoformat()
         self.__dict__['__class__'] = 'BaseModel'
         return self.__dict__

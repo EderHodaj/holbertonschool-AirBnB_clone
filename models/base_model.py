0030348a850ee86ba35aa2de003d3924df640969
EderHodaj <eder0302@gmail.com>
2024-02-26 19:05:20 +0000

    FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import uuid
 import datetime
+from __init__ import storage
+
 '''Create Base Classe'''
 
 
@@ -15,6 +17,7 @@ class BaseModel:
                     value = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 setattr(self,key,value)
         else:
+            storage.new()
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
 
@@ -23,6 +26,7 @@ class BaseModel:
 
     def save(self):
         self.updated_at = datetime.datetime.now()
+        storage.save()
 
     def to_dict(self):
         self.created_at = datetime.datetime.now().isoformat()
---
+++ models/engine/file_storage.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import json
+'''
+Create FileStorage class
+'''
+
+
+class FileStorage:
+    def __init__(self):
+        self.__file_path = ""
+        self.__objects = {}
+  
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        self.__objects["BaseModel.id"] = "obj"
+
+    def save(self):
+        with open(self.__file_path, "w") as json_file:
+            json.dump(self.__objects, json_file)
+
+    def reload(self):
+        if self.__file_path:
+            with open(self.__file_path, "r") as json_file:
+                json.load(json_file)
+        else:
+            pass
+

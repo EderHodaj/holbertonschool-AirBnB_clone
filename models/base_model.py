6ff8583f88f2978b0822cacceadca3aa9d917764
EderHodaj <eder0302@gmail.com>
2024-02-26 11:14:53 +0000

    BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import uuid
+import datetime
+'''Create Base Classe'''
+
+
+class BaseModel:
+    '''Createing the class attributes'''
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now().isoformat()
+        self.updated_at = datetime.datetime.now().isoformat()
+
+    def __str__(self):
+        return f"[BaseModel] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        self.__dict__['__class__'] = 'BaseModel'
+        return self.__dict__

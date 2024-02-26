bae10b2ec83a7e4b006362b215fef6be471b77a3
eneamilo91 <eneamilo514@gmail.com>
2024-02-26 14:34:36 +0100

    added file

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,18 @@ import datetime
 
 class BaseModel:
     '''Createing the class attributes'''
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    self.created_at = datetime.datetime.now()
+                    self.updated_at = datetime.datetime.now()
+                setattr(self,key,value)          
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
 
     def __str__(self):
         return f"[BaseModel] ({self.id}) {self.__dict__}"
@@ -22,3 +30,25 @@ class BaseModel:
         self.updated_at = datetime.datetime.now().isoformat()
         self.__dict__['__class__'] = 'BaseModel'
         return self.__dict__
+
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
+
+print("--")
+print(my_model is my_new_model)

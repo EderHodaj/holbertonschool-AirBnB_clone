73d2796c49d26091a683b80df7a72346eb55f55a
EderHodaj <eder0302@gmail.com>
2024-02-27 10:32:02 +0000

    New_update_import

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,18 +28,7 @@ class FileStorage:
             json_dict[key] = value.to_dict()
         with open(FileStorage.__file_path, "w") as json_file:
             json.dump(json_dict, json_file)
-'''
-    def reload(self):
-        """deserializes dictionaries from json file"""
-        try:
-            with open(FileStorage.__file_path, "r") as json_file:
-                from_json = json.load(json_file)
-                for key, value in from_json.items():
-                    cls_name = eval(value["__class__"])(**value)
-                    self.__objects[key] = cls_name
-        except FileNotFoundError:
-            return
-'''
+
     def reload(self):
         """deserializes the JSON file to __objects
         (only if the JSON file (__file_path) exists;

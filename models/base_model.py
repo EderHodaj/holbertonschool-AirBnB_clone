385d0b4b4795e1c650102bf46f03ce851240109e
eneamilo91 <eneamilo514@gmail.com>
2024-03-01 23:17:18 +0000

    updated several files and optimized them

--- models/base_model.py
+++ models/base_model.py
@@ -23,7 +23,7 @@ class BaseModel:
             
 
     def __str__(self):
-        return f"[BaseModel] ({self.id}) {self.__dict__}"
+        return f"[self.__class__.__name__] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.datetime.now()
